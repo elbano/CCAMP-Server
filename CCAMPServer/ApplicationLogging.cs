8ef2828cb6f5c97e76b573cd6971af0e727a57c4
elbano <elbano28@gmail.com>
2018-09-04 17:40:47 -0400

    Server project creation with logger

---
+++ CCAMPServer/ApplicationLogging.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ILogger = Serilog.ILogger;
+
+namespace CCAMPServer
+{
+    public class ApplicationLogging
+    {
+        private static ILogger logger;
+
+        public static void ConfigureLogger(IConfiguration configuration)
+        {
+            logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        public static ILogger Logger
+        {
+            get
+            {                
+                return logger;
+            }
+        }
+    }
+}
